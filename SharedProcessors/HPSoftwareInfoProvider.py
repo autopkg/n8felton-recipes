eb396d34ba3107f953970db31e5326294304c56f
Glynn Lane <primalcurve@gmail.com>
2022-12-22 12:52:59 -0600

    Overhauling HPPrinterDrivers (#175)
    * Overhauling HPPrinterDrivers
    
    Due to some changes on HP's side, the HPPrinterDrivers recipe is not functioning. I have made some changes in order to fix those errors and provide greater flexibility in the recipe.
    
    ### HPSoftwareInfoProvider.py
    
    * Added additional imports
    * Removed Python 2 workaround
    * Modified HP_BASE_URL to also have a key for the query type
    * Added optional query
    * Fixed references to case-sensitive environment variables
    * Added new HP_QUERY_TYPE environment variable
    * Modified "OS" to be `operating_system` to prevent conflicts with `os` built-in.
    * Changed "product_number" to "HP_QUERY" as it's ProductNumber sometimes and ModelName otherwise
    * Used the packaging versioner to parse the version strings and figure out which is higher. It is *not* always the first one in the new schema.
    * Verified if the discovered url begins with `ftp` and modify it to use `https` since the HP FTP server rejects FTP requests now. Even the HP Easy Admin client tries HTTPS first.
    
    ### HPPrinterDrivers.download.recipe
    * Modified to use the above changes.
    
    * Adding some additional error handling
    
    * Enhancing legacy support
    
    * Auto format
    
    Co-authored-by: Nathan Felton <n8felton@gmail.com>

--- SharedProcessors/HPSoftwareInfoProvider.py
+++ SharedProcessors/HPSoftwareInfoProvider.py
@@ -18,51 +18,66 @@
 
 import json
 
-from autopkglib import Processor, ProcessorError, URLGetter
+from autopkglib import ProcessorError, URLGetter
+from pkg_resources import packaging
+from urllib.parse import urlparse, quote
 
-try:
-    from urllib.parse import quote  # For Python 3
-except ImportError:
-    from urllib2 import quote  # For Python 2
 
 __all__ = ["HPSoftwareInfoProvider"]
 
 HP_BASE_URL = (
     "https://h20614.www2.hp.com/ediags/solutions/software/v3?"
-    "ProductNumber={product_number}&"
-    "OS={os}&"
+    "{hp_query_type}={hp_query}&"
+    "OS={operating_system}&"
     "lc={lang_code}&"
     "cc={country_code}"
 )
 
+HP_OPTIONAL_QUERY = "&client=hp-quick-start"
+
 
 class HPSoftwareInfoProvider(URLGetter):
     """Determines the ESSENTIAL-REQUIRED software necessary for a given
-    product_number.
+    hp_query.
 
     Returns the URL, version and description of the software.
     """
 
     description = __doc__
     input_variables = {
-        "product_number": {
+        "HP_QUERY_TYPE": {
             "required": True,
+            "default": "ModelName",
             "description": (
-                "HP Product Number of device that requires software. "
-                "Typically 6 alphanumeric characters, e.g. E6B70A"
+                "Query type for specific product. For legacy products, this "
+                "is ProductNumber, for future products, this is ModelName. "
+                "Default is ModelName."
             ),
         },
-        "os": {
-            "required": False,
-            "default": "Mac OS X 10.11",
-            "description": "The OS you want to search for",
+        "HP_QUERY": {
+            "required": True,
+            "default": "HP Color LaserJet Pro M453-4",
+            "description": (
+                "HP Product Number or Model Name of device that requires "
+                "software. For ProductNumber, this is typically 6 "
+                "alphanumeric characters, e.g. E6B70A. For ModelName, this "
+                "is the full product name, e.g. HP Color LaserJet Pro M453-4."
+            ),
         },
-        "lang_code": {
+        "OPERATING_SYSTEM": {
+            "required": True,
+            "default": "macOS 12.0",
+            "description": (
+                "Full name of the OS you want to search for, "
+                "e.g. macOS 12.0. Default is macOS 12.0"
+            ),
+        },
+        "LANG_CODE": {
             "required": False,
             "default": "en",
             "description": "ISO 639-1 code for preferred language",
         },
-        "country_code": {
+        "COUNTRY_CODE": {
             "required": False,
             "default": "us",
             "description": "ISO 3166-1 alpha-2 code for preferred country",
@@ -84,35 +99,65 @@ class HPSoftwareInfoProvider(URLGetter):
         """Main process."""
 
         # Capture input variables
-        product_number = self.env.get("product_number")
-        os = quote(self.env.get("os", "Mac OS X 10.11"))
-        lang_code = self.env.get("lang_code", "en")
-        country_code = self.env.get("country_code", "us")
+        # Fallback to PRODUCT_NUMBER for legacy support.
+        # OPERATING_SYSTEM will probably need to be set to 'Mac OS X 10.11'
+        if self.env.get("PRODUCT_NUMBER"):
+            hp_query_type = "ProductNumber"
+            hp_query = (self.env.get("PRODUCT_NUMBER")).replace(" ", "+")
+        else:
+            hp_query_type = self.env.get("HP_QUERY_TYPE", "ModelName")
+            hp_query = (self.env.get("HP_QUERY")).replace(" ", "+")
+        operating_system = quote(self.env.get("OPERATING_SYSTEM"))
+        lang_code = self.env.get("LANG_CODE", "en")
+        country_code = self.env.get("COUNTRY_CODE", "us")
 
         # Determine software URL
         software_url = HP_BASE_URL.format(
-            product_number=product_number,
-            os=os,
+            hp_query_type=hp_query_type,
+            hp_query=hp_query,
+            operating_system=operating_system,
             lang_code=lang_code,
             country_code=country_code,
         )
-        self.output("Software URL: {software_url}".format(software_url=software_url), 2)
+        # Add the additional optional query string to the URL when searching
+        # for ModelName
+        if hp_query_type == "ModelName":
+            software_url = software_url + HP_OPTIONAL_QUERY
+
+        self.output(f"Software URL: {software_url}", 2)
 
         # Get software list and iterate through it
         software_list = self.get_hp_software_list(software_url)
         self.output(software_list, 3)
+        # Create a packaging.version.Version object with all zeroes.
+        greatest_version = packaging.version.parse("0.0.0.0")
+        recent_software = None
         for software in software_list:
-            if software["Type"] == "ESSENTIAL-REQUIRED":
-                self.env["url"] = software["FtpURL"]
-                self.output(software["FtpURL"])
-                self.env["version"] = software["Version"]
-                self.env["description"] = software["Description"]
-                # We're hoping there is only one ESSENTIAL-REQUIRED
-                # but if there are multiple, we should only return the first.
-                # If this becomes a problem, we'll need to test on some of the
-                # other keys that are available.
-                break
-        # essential_required_software = software_list
+            # Skip any key that isn't "ESSENTIAL-REQUIRED"
+            if software["Type"] != "ESSENTIAL-REQUIRED":
+                continue
+            software_version = packaging.version.parse(software["Version"])
+            # Find the greatest version of the HP Essentials.
+            if greatest_version < software_version:
+                greatest_version = software_version
+                recent_software = software
+
+        if recent_software is None:
+            raise ProcessorError("Unable to find valid version of HP software")
+        parsed_url = urlparse(recent_software["FtpURL"])
+
+        # The HP ftp server rejects FTP requests now.
+        if parsed_url.scheme == "ftp":
+            # urllib.parse.ParseResult is a subclass of namedtuple. So it
+            # is iterable.
+            flat_url = "https://" + "".join([_u for _u in parsed_url[1:]])
+        else:
+            flat_url = parsed_url.geturl()
+
+        self.env["url"] = flat_url
+        self.output(flat_url)
+        self.env["version"] = recent_software["Version"]
+        self.env["description"] = recent_software["Description"]
 
 
 if __name__ == "__main__":
